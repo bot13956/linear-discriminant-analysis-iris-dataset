624d8448ba5fa7dcb60cac0203affbd00d0544b8
Benjamin Obi Tayo <benjaminobi@gmail.com>
2018-11-06 15:27:11 -0600

    Add files via upload

---
+++ LDA_irisdataset.py
@@ -0,0 +1,113 @@
+
+"""
+
+Created on Tue Nov  6 15:02:10 2018
+
+@author: btayo
+"""
+#Import necessary libraries
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+np.set_printoptions(precision=4)
+
+
+#Read dataset
+file_path = "https://raw.githubusercontent.com/bot13956/linear-discriminant-analysis-iris-dataset/master/iris.data.csv"
+df = pd.read_csv(file_path, header=None)
+df.head()
+
+#Encode categorical class labels
+from sklearn.preprocessing import LabelEncoder
+class_le = LabelEncoder()
+y = class_le.fit_transform(df[4].values)
+
+
+#Standardize features
+from sklearn.preprocessing import StandardScaler
+stdsc = StandardScaler()
+X_train_std = stdsc.fit_transform(df.iloc[:,range(0,4)].values)
+
+
+# Construct within-class covariant scatter matrix S_W
+S_W = np.zeros((4,4))
+for i in range(3):
+    S_W += np.cov(X_train_std[y==i].T)
+    
+
+#Construct between-class scatter matrix S_B
+N=np.bincount(y) # number of samples for given class
+vecs=[]
+[vecs.append(np.mean(X_train_std[y==i],axis=0)) for i in range(3)] # class means
+mean_overall = np.mean(X_train_std, axis=0) # overall mean
+S_B=np.zeros((4,4))
+for i in range(3):
+    S_B += N[i]*(((vecs[i]-mean_overall).reshape(4,1)).dot(((vecs[i]-mean_overall).reshape(1,4))))
+    
+
+#Calculate sorted eigenvalues and eigenvectors of inverse(S_W)dot(S_B)
+eigen_vals, eigen_vecs = np.linalg.eig(np.linalg.inv(S_W).dot(S_B))
+eigen_pairs = [(np.abs(eigen_vals[i]), eigen_vecs[:,i]) for i in range(len(eigen_vals))]
+eigen_pairs = sorted(eigen_pairs,key=lambda k: k[0], reverse=True)
+print('Eigenvalues in decreasing order:\n')
+for eigen_val in eigen_pairs:
+    print(eigen_val[0])
+    
+
+#Plot main LDA components
+tot = sum(eigen_vals.real)
+discr = [(i / tot) for i in sorted(eigen_vals.real, reverse=True)]
+cum_discr = np.cumsum(discr)
+plt.bar(range(1, 5), discr, width=0.2,alpha=0.5, align='center',label='individual "discriminability"')
+plt.step(range(1, 5), cum_discr, where='mid',label='cumulative "discriminability"')
+plt.ylabel('"discriminability" ratio')
+plt.xlabel('Linear Discriminants')
+plt.ylim([-0.1, 1.1])
+plt.legend(loc='best')
+plt.show()
+
+
+#Project original features onto the new feature space
+W=np.hstack((eigen_pairs[0][1][:, ].reshape(4,1),eigen_pairs[1][1][:, ].reshape(4,1))).real
+X_train_lda = X_train_std.dot(W)
+
+
+#Plot transformed features in LDA subspace
+data=pd.DataFrame(X_train_lda)
+data['class']=y
+data.columns=["LD1","LD2","class"]
+data.head()
+
+markers = ['s', 'x','o']
+sns.lmplot(x="LD1", y="LD2", data=data, markers=markers,fit_reg=False, hue='class', legend=False)
+plt.legend(loc='upper center')
+plt.show()
+
+
+#LDA implementation using scikit-learn
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+lda = LinearDiscriminantAnalysis(n_components=2)
+X_train_lda = lda.fit_transform(X_train_std, y)
+
+data=pd.DataFrame(X_train_lda)
+data['class']=y
+data.columns=["LD1","LD2","class"]
+data.head()
+
+markers = ['s', 'x','o']
+colors = ['r', 'b','g']
+sns.lmplot(x="LD1", y="LD2", data=data, hue='class', markers=markers,fit_reg=False,legend=False)
+plt.legend(loc='upper center')
+plt.show()
+
+
+
+
+
+
+
+
+
+
+
